414218c68e5f92300063ee8876e4d62c2b7c14b0
DuongDang0605 <dang123123258258@gmail.com>
2026-03-06 16:09:29 +0700

    filter and list user

--- src/main/java/org/example/studyhub/reponsitory/SettingRepository.java
+++ src/main/java/org/example/studyhub/reponsitory/SettingRepository.java
@@ -2,6 +2,10 @@ package org.example.studyhub.reponsitory;
 
 import org.example.studyhub.model.Setting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SettingRepository extends JpaRepository<Setting,Long> {
+
+    @Query("select s from Setting s where s.id = :roleId ")
+    Setting findRoleByRoleId(Long roleId);
 }
