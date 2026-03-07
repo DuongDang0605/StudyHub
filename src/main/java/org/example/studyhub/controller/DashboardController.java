d9ec0d4d056a54716ef10b12501f451d0c486196
DuongDang0605 <dang123123258258@gmail.com>
2026-03-08 00:49:55 +0700

    UI Boostrap 5

--- src/main/java/org/example/studyhub/controller/DashboardController.java
+++ src/main/java/org/example/studyhub/controller/DashboardController.java
@@ -1,4 +1,15 @@
 package org.example.studyhub.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dash")
 public class DashboardController {
+    @GetMapping
+    public String getDash(){
+        return "admin/dashboard/dashboard";
+    }
+
 }
--- src/main/java/org/example/studyhub/controller/HomeController.java
+++ src/main/java/org/example/studyhub/controller/HomeController.java
@@ -1,12 +1,14 @@
 package org.example.studyhub.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-    public String home() {
-        return "homepage/homepage";
+    @GetMapping
+    public String getHome() {
+        return "index";
     }
 }
