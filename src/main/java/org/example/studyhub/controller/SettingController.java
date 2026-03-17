63c5ccf238d950a48e1bf9b68c8ef47da49fa429
pcuongmc72 <mcphucuongng72@gmail.com>
2026-03-17 07:11:11 +0700

    setting list

--- src/main/java/org/example/studyhub/controller/SettingController.java
+++ src/main/java/org/example/studyhub/controller/SettingController.java
@@ -1,10 +1,27 @@
 package org.example.studyhub.controller;
 
 
+import org.example.studyhub.model.Setting;
+import org.example.studyhub.repository.SettingRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/setting")
 public class SettingController {
+    private final SettingRepository settingRepository;
+
+    public SettingController(SettingRepository settingRepository) {
+        this.settingRepository = settingRepository;
+    }
+    @GetMapping
+    public String setting(Model model){
+        List<Setting> settings =settingRepository.findByTypeIsNotNullOrderByIdAsc();
+        model.addAttribute("settings", settings);
+        return "admin/setting/setting-list";
+    }
 }
--- src/main/java/org/example/studyhub/repository/SettingRepository.java
+++ src/main/java/org/example/studyhub/repository/SettingRepository.java
@@ -3,6 +3,9 @@ package org.example.studyhub.repository;
 import org.example.studyhub.model.Setting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -12,5 +15,30 @@ public interface SettingRepository extends JpaRepository<Setting, Long> {
     List<Setting> getAllRoles();
 
     List<Setting> findByTypeIdAndStatus(Long typeId, String status);
+    List<Setting> findByTypeIsNotNullOrderByIdAsc();
 
+
+    List<Setting> findByTypeIsNullAndStatusOrderByNameAsc(String status);
+
+    @Query(value = """
+   SELECT s
+   FROM Setting s
+   LEFT JOIN s.type t
+   WHERE s.type IS NOT NULL
+     AND (:typeId IS NULL OR t.id = :typeId)
+     AND (:status IS NULL OR s.status = :status)
+   ORDER BY s.id ASC
+   """,
+            countQuery = """
+   SELECT COUNT(s)
+   FROM Setting s
+   LEFT JOIN s.type t
+   WHERE s.type IS NOT NULL
+     AND (:typeId IS NULL OR t.id = :typeId)
+     AND (:status IS NULL OR s.status = :status)
+   """)
+    Page<Setting> searchSettings(@Param("typeId") Long typeId,
+                                 @Param("status") String status,
+                                 @Param("keyword") String keyword,
+                                 Pageable pageable);
 }
